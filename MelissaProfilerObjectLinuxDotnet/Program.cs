9a69cc584d29fed07599ce8b9414509e8e68b3fc
Melissa <ha@melissadata.com>
2024-01-26 11:05:45 -0800

    Commit .NET C# code for Profiler Object in Linux

---
+++ MelissaProfilerObjectLinuxDotnet/Program.cs
@@ -0,0 +1,318 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using MelissaData;
+
+
+namespace MelissaProfilerObjectLinuxDotnet
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Variables
+      string license = "";
+      string testFile = "";
+      string dataPath = "";
+
+      ParseArguments(ref license, ref testFile, ref dataPath, args);
+      RunAsConsole(license, testFile, dataPath);
+    }
+
+    static void ParseArguments(ref string license, ref string testFile, ref string dataPath, string[] args)
+    {
+      for (int i = 0; i < args.Length; i++)
+      {
+        if (args[i].Equals("--license") || args[i].Equals("-l"))
+        {
+          if (args[i + 1] != null)
+          {
+            license = args[i + 1];
+          }
+        }
+        if (args[i].Equals("--file") || args[i].Equals("-f"))
+        {
+          if (args[i + 1] != null)
+          {
+            testFile = args[i + 1];
+          }
+        }
+        if (args[i].Equals("--dataPath") || args[i].Equals("-d"))
+        {
+          if (args[i + 1] != null)
+          {
+            dataPath = args[i + 1];
+          }
+        }
+      }
+    }
+
+    static void RunAsConsole(string license, string testFile, string dataPath)
+    {
+      Console.WriteLine("\n\n============== WELCOME TO MELISSA PROFILER OBJECT LINUX DOTNET ==============\n");
+      
+      ProfilerObject profilerObject = new ProfilerObject(license, dataPath);
+
+      bool shouldContinueRunning = true;
+
+      if (profilerObject.mdProfilerObj.GetInitializeErrorString() != "No error.")
+      {
+        shouldContinueRunning = false;
+      }
+
+      while (shouldContinueRunning)
+      {
+        DataContainer dataContainer = new DataContainer();
+
+        if (string.IsNullOrEmpty(testFile))
+        {
+          Console.WriteLine("\nFill in each value to see the Profiler Object results");
+
+          Console.Write("File Path: ");
+          dataContainer.InputFile = Console.ReadLine();
+        }
+        else
+        {
+          dataContainer.InputFile = testFile;
+        }
+
+        // Print user input
+        Console.WriteLine("\n=================================== INPUTS ==================================\n");
+
+        List<string> sections = dataContainer.GetWrapped(dataContainer.InputFile, 50);
+
+        Console.WriteLine($"\t                Input File: {sections[0]}");
+
+        for (int i = 1; i < sections.Count; i++)
+        {
+          if ((i == sections.Count - 1) && sections[i].EndsWith("/"))
+          {
+            sections[i] = sections[i].Substring(0, sections[i].Length - 1);
+          }
+          Console.WriteLine($"\t                            {sections[i]}");
+        }
+
+        // Execute Profiler Object
+        profilerObject.ExecuteObjectAndResultCodes(ref dataContainer);
+
+        // Print output
+        Console.WriteLine("\n=================================== OUTPUT ==================================\n");
+        Console.WriteLine("\n\tProfiler Object Information:");
+        Console.WriteLine("\n\t        TABLE STATISTICS\n\n");
+        Console.WriteLine("\t         TableRecordCount           :  {0}", profilerObject.mdProfilerObj.GetTableRecordCount());
+        Console.WriteLine("\t         ColumnCount                :  {0}", profilerObject.mdProfilerObj.GetColumnCount());
+        Console.WriteLine("");
+        Console.WriteLine("\t         ExactMatchDistinctCount    :  {0}", profilerObject.mdProfilerObj.GetTableExactMatchDistinctCount());
+        Console.WriteLine("\t         ExactMatchDupesCount       :  {0}", profilerObject.mdProfilerObj.GetTableExactMatchDupesCount());
+        Console.WriteLine("\t         ExactMatchLargestGroup     :  {0}", profilerObject.mdProfilerObj.GetTableExactMatchLargestGroup());
+        Console.WriteLine("");
+        Console.WriteLine("\t         ContactMatchDistinctCount  :  {0}", profilerObject.mdProfilerObj.GetTableContactMatchDistinctCount());
+        Console.WriteLine("\t         ContactMatchDupesCount     :  {0}", profilerObject.mdProfilerObj.GetTableContactMatchDupesCount());
+        Console.WriteLine("\t         ContactMatchLargestGroup   :  {0}", profilerObject.mdProfilerObj.GetTableContactMatchLargestGroup());
+        Console.WriteLine("");
+        Console.WriteLine("\t         HouseholdMatchDistinctCount:  {0}", profilerObject.mdProfilerObj.GetTableHouseholdMatchDistinctCount());
+        Console.WriteLine("\t         HouseholdMatchDupesCount   :  {0}", profilerObject.mdProfilerObj.GetTableHouseholdMatchDupesCount());
+        Console.WriteLine("\t         HouseholdMatchLargestGroup :  {0}", profilerObject.mdProfilerObj.GetTableHouseholdMatchLargestGroup());
+        Console.WriteLine("");
+        Console.WriteLine("\t         AddressMatchDistinctCount  :  {0}", profilerObject.mdProfilerObj.GetTableAddressMatchDistinctCount());
+        Console.WriteLine("\t         AddressMatchDupesCount     :  {0}", profilerObject.mdProfilerObj.GetTableAddressMatchDupesCount());
+        Console.WriteLine("\t         AddressMatchLargestGroup   :  {0}", profilerObject.mdProfilerObj.GetTableAddressMatchLargestGroup());
+
+        Console.WriteLine("\n\n\t        COLUMN STATISTICS\n\n");
+
+        // STATE Iterator Example
+        Console.WriteLine("\t         STATE Value        Count");
+        profilerObject.mdProfilerObj.StartDataFrequency("state", mdProfiler.Order.OrderCountAscending);
+        do
+        {
+          string state = profilerObject.mdProfilerObj.GetDataFrequencyValue("state").ToString();
+          string count = profilerObject.mdProfilerObj.GetDataFrequencyCount("state").ToString();
+
+          Console.WriteLine($"\t              {state,-16}{count,-10}");
+
+        } while (profilerObject.mdProfilerObj.GetNextDataFrequency("state") == 1);
+        Console.WriteLine("");
+
+
+        // POSTAL Iterator Example
+        Console.WriteLine("\t         POSTAL Pattern     Count");
+        profilerObject.mdProfilerObj.StartPatternFrequency("zip", mdProfiler.Order.OrderCountAscending);
+        do
+        {
+          string zipCode = profilerObject.mdProfilerObj.GetPatternFrequencyValue("zip").ToString();
+          string count = profilerObject.mdProfilerObj.GetPatternFrequencyCount("zip").ToString();
+
+          Console.WriteLine($"\t              {zipCode,-16}{count,-10}");
+
+        } while (profilerObject.mdProfilerObj.GetNextPatternFrequency("zip") == 1);
+
+        bool isValid = false;
+        if (!string.IsNullOrEmpty(testFile))
+        {
+          isValid = true;
+          shouldContinueRunning = false;
+        }
+        while (!isValid)
+        {
+          Console.WriteLine("\nTest another file? (Y/N)");
+          string testAnotherResponse = Console.ReadLine();
+
+          if (!string.IsNullOrEmpty(testAnotherResponse))
+          {
+            testAnotherResponse = testAnotherResponse.ToLower();
+            if (testAnotherResponse == "y")
+            {
+              isValid = true;
+            }
+            else if (testAnotherResponse == "n")
+            {
+              isValid = true;
+              shouldContinueRunning = false;
+            }
+            else
+            {
+              Console.Write("Invalid Response, please respond 'Y' or 'N'");
+            }
+          }
+        }
+      }
+      profilerObject.mdProfilerObj.Dispose();
+
+      Console.WriteLine("\n================== THANK YOU FOR USING MELISSA DOTNET OBJECT ================\n");
+    }
+  }
+
+  class ProfilerObject
+  {
+    // Path to Profiler Object data files (.dat, etc)
+    string dataFilePath;
+
+    // Create instance of Melissa Profiler Object
+    public mdProfiler mdProfilerObj = new mdProfiler();
+
+    public ProfilerObject(string license, string dataPath)
+    {
+      // Set license string and set path to data files (.dat, etc)
+      mdProfilerObj.SetLicenseString(license);
+      mdProfilerObj.SetFileName("testFile.prf");
+      mdProfilerObj.SetAppendMode(mdProfiler.AppendMode.Overwrite);
+
+      dataFilePath = dataPath;
+      mdProfilerObj.SetPathToProfilerDataFiles(dataFilePath);
+
+      mdProfilerObj.SetSortAnalysis(1); // the default is 1
+      mdProfilerObj.SetMatchUpAnalysis(1);// the default is 1
+      mdProfilerObj.SetRightFielderAnalysis(1);// the default is 1
+      mdProfilerObj.SetDataAggregation(1);// the default is 1
+
+      // If you see a different date than expected, check your license string and either download the new data files or use the Melissa Updater program to update your data files.  
+      mdProfiler.ProgramStatus pStatus = mdProfilerObj.InitializeDataFiles();
+
+      if (pStatus != mdProfiler.ProgramStatus.ErrorNone)
+      {
+        Console.WriteLine("Failed to Initialize Object.");
+        Console.WriteLine(pStatus);
+        return;
+      }
+
+      Console.WriteLine($"                       DataBase Date: {mdProfilerObj.GetDatabaseDate()}");
+      Console.WriteLine($"                     Expiration Date: {mdProfilerObj.GetLicenseExpirationDate()}");
+
+      /**
+       * This number should match with the file properties of the Melissa Object binary file.
+       * If TEST appears with the build number, there may be a license key issue.
+       */
+      Console.WriteLine($"                      Object Version: {mdProfilerObj.GetBuildNumber()}\n");
+    }
+
+    // This will call the lookup function to process the input address as well as generate the result codes
+    public void ExecuteObjectAndResultCodes(ref DataContainer data)
+    {
+      mdProfilerObj.AddColumn("first", mdProfiler.ProfilerColumnType.ColumnTypeVariableUnicodeString, mdProfiler.ProfilerDataType.DataTypeFirstName);
+      mdProfilerObj.AddColumn("last", mdProfiler.ProfilerColumnType.ColumnTypeVariableUnicodeString, mdProfiler.ProfilerDataType.DataTypeLastName);
+      mdProfilerObj.AddColumn("address", mdProfiler.ProfilerColumnType.ColumnTypeVariableUnicodeString, mdProfiler.ProfilerDataType.DataTypeAddress);
+      mdProfilerObj.AddColumn("city", mdProfiler.ProfilerColumnType.ColumnTypeVariableUnicodeString, mdProfiler.ProfilerDataType.DataTypeCity);
+      mdProfilerObj.AddColumn("state", mdProfiler.ProfilerColumnType.ColumnTypeVariableUnicodeString, mdProfiler.ProfilerDataType.DataTypeStateOrProvince);
+      mdProfilerObj.AddColumn("zip", mdProfiler.ProfilerColumnType.ColumnTypeVariableUnicodeString, mdProfiler.ProfilerDataType.DataTypeZipOrPostalCode);
+
+      string[] records = null;
+      try
+      {
+        records = File.ReadAllLines(data.InputFile, Encoding.UTF8);
+      }
+      catch (Exception)
+      {
+        Console.WriteLine("Error: Unable to open the input file");
+        Environment.Exit(1);
+      }
+
+      mdProfilerObj.StartProfiling();
+
+      // Inputting the records to the Profiler Object
+      foreach (string record in records)
+      {
+        string[] fields = record.Split(new char[] { ',' });
+        mdProfilerObj.SetColumn("first", fields[0]);
+        mdProfilerObj.SetColumn("last", fields[1]);
+        mdProfilerObj.SetColumn("address", fields[2]);
+        mdProfilerObj.SetColumn("city", fields[3]);
+        mdProfilerObj.SetColumn("state", fields[4]);
+        mdProfilerObj.SetColumn("zip", fields[5]);
+
+        mdProfilerObj.AddRecord();
+      }
+
+      mdProfilerObj.ProfileData();
+
+      // ResultsCodes explain any issues Profiler Object has with the object.
+      // List of result codes for Profiler Object
+      // https://wiki.melissadata.com/index.php?title=Result_Code_Details#Profiler_Object
+    }
+  }
+
+  public class DataContainer
+  {
+    public string InputFile { get; set; } = "";
+
+    public List<string> GetWrapped(string path, int maxLineLength)
+    {
+      FileInfo file = new FileInfo(path);
+      string filePath = file.FullName;
+
+      string[] lines = filePath.Split(new string[] { "/" }, StringSplitOptions.None);
+      string currentLine = "";
+      List<string> wrappedString = new List<string>();
+
+      foreach (string section in lines)
+      {
+        if ((currentLine + section).Length > maxLineLength)
+        {
+          wrappedString.Add(currentLine.Trim());
+          currentLine = "";
+        }
+
+        if (section.Contains(path))
+        {
+          currentLine += section;
+        }
+        else
+        {
+          currentLine += section + "/";
+        }
+      }
+
+      if (currentLine.Length > 0)
+      {
+        wrappedString.Add(currentLine.Trim());
+      }
+
+      return wrappedString;
+    }
+
+  }
+}
+
---
+++ README.md
@@ -0,0 +1,181 @@
+# Melissa - Profiler Object Linux Dotnet
+
+## Purpose
+This code showcases the Melissa Profiler Object using C#.
+
+Please feel free to copy or embed this code to your own project. Happy coding!
+
+For the latest Melissa Profiler Object release notes, please visit: https://releasenotes.melissa.com/on-premise-api/profiler-object/
+
+For further details, please visit: https://docs.melissa.com/on-premise-api/profiler-object/profiler-object-quickstart.html
+
+The console will ask the user for:
+
+- A csv that contains data that you would like to profile
+
+And return 
+
+- TableRecordCount
+- ColumnCount
+- ExactMatchDistinctCount
+- ExactMatchDupesCount
+- ExactMatchLargestGroup
+- ContactMatchDistinctCount
+- ContactMatchDupesCount
+- ContactMatchLargestGroup
+- HouseholdMatchDistinctCount
+- HouseholdMatchDupesCount
+- HouseholdMatchLargestGroup
+- AddressMatchDistinctCount
+- AddressMatchDupesCount
+- AddressMatchLargestGroup
+- States and Counts
+- Postal Patterns and Counts
+
+## Tested Environments
+- Linux 64-bit .NET 8.0, Ubuntu 20.04.05 LTS
+- Melissa data files for 2023-12
+
+## Required File(s) and Programs
+#### Binaries
+This is the c++ code of the Melissa Object.
+
+- libmdProfiler.so
+
+#### Data File(s)
+- mdProfiler.dat
+- mdProfiler.mc
+- mdProfiler.cfg
+
+## Getting Started
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+#### Install the Dotnet Core SDK
+Before starting, check to see if you already have the .NET 8.0 SDK already installed by entering this command:
+
+`dotnet --list-sdks`
+
+If the .NET 8.0 SDK is already installed, you should see it in the following list:
+
+![alt text](/screenshots/dotnet_output.png)
+
+As long as the above list contains version `8.0.xxx` (underlined in red), then you can skip to the next step. If your list does not contain version 8.0, or you get any kind of error message, then you will need to download and install the .NET 8.0 SDK.
+
+To download, run the following commands to add the Microsoft package signing key to your list of trusted keys and add the package repository.
+
+```
+wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+```
+
+Next, you can now run this command to install the .NET 8.0 SDK:
+
+```
+sudo apt-get update && \
+  sudo apt-get install -y dotnet-sdk-8.0
+```
+
+Once all of this is done, you should be able to verify that the SDK is installed with the `dotnet --list-sdks` command.
+
+----------------------------------------
+
+#### Download this project
+```
+$ git clone https://github.com/MelissaData/ProfilerObject-Dotnet-Linux
+$ cd ProfilerObject-Dotnet-Linux
+```
+
+#### Set up Melissa Updater 
+Melissa Updater is a CLI application allowing the user to update their Melissa applications/data. 
+
+- In the root directory of the project, create a folder called `MelissaUpdater` by using the command: 
+
+  `mkdir MelissaUpdater`
+
+- Enter the newly created folder using the command:
+
+  `cd MelissaUpdater`
+
+- Proceed to install the Melissa Updater using the curl command: 
+
+  `curl -L -O https://releases.melissadata.net/Download/Library/LINUX/NET/ANY/latest/MelissaUpdater`
+
+- After the Melissa Updater is installed, you will need to change the Melissa Updater to an executable using the command:
+
+  `chmod +x MelissaUpdater`
+
+- Now that the Melissa Updater is set up, you can now proceed to move back into the project folder by using the command:
+  
+   `cd ..`
+----------------------------------------
+
+#### Different ways to get data file(s)
+1.  Using Melissa Updater
+	- It will handle all of the data download/path and .so file(s) for you. 
+2.  If you already have the latest Release (ZIP), you can find the data file(s) and .so file(s) in there
+	- Use the location of where you copied/installed the data and update the "DataPath" variable in the bash script.
+	- Copy all the .so file(s) mentioned above into the `MelissaProfilerObjectLinuxDotnet` project folder.
+
+#### Change Bash Script Permissions
+To be able to run the bash script, you must first make it an executable using the command:
+
+`chmod +x MelissaProfilerObjectLinuxDotnet.sh`
+
+As an indicator, the filename will change colors once it becomes an executable.
+
+## Run Bash Script
+Parameters:
+- -f or --file: a test csv file
+
+  This is convenient when you want to get results for a specific csv file in one run instead of testing multiple csv files in interactive mode.
+
+- -l or --license (optional): a license string to test the Profiler Object 
+- -q or --quiet (optional): add to command if you do not want to get any console output from the Melissa Updater
+
+  When you have modified the script to match your data location, let's run the script. There are two modes:
+- Interactive
+    
+  The script will prompt the user for a csv file, then use the provided csv file to test Profiler Object. For example:
+    ```
+    $ ./MelissaProfilerObjectLinuxDotnet.sh
+    ```
+    For quiet mode:
+    ```
+    $ ./MelissaProfilerObjectLinuxDotnet.sh --quiet
+    ```
+
+- Command Line
+
+  You can pass a csv file and a license string into the `--file` and `--license` parameters respectively to test Profiler Object. For example:
+
+    With all parameters:
+    ```
+    $ ./MelissaProfilerObjectLinuxDotnet.sh --file "MelissaProfilerObjectSampleInput.csv"
+    $ ./MelissaProfilerObjectLinuxDotnet.sh --file "MelissaProfilerObjectSampleInput.csv" --license "<your_license_string>"
+    ```
+    For quiet mode:
+    ```
+    $ ./MelissaProfilerObjectLinuxDotnet.sh --file "MelissaProfilerObjectSampleInput.csv" --quiet
+    $ ./MelissaProfilerObjectLinuxDotnet.sh --file "MelissaProfilerObjectSampleInput.csv" --license "<your_license_string>" --quiet
+    ```
+This is the expected outcome of a successful setup for interactive mode:
+
+![alt text](/screenshots/output.png)
+
+## Troubleshooting
+Troubleshooting for errors found while running your program.
+
+### C# Errors:
+| Error      | Description |
+| ----------- | ----------- |
+| ErrorRequiredFileNotFound      | Program is missing a required file. Please check your Data folder and refer to the list of required files above. If you are unable to obtain all required files through the Melissa Updater, please contact technical support below. |
+| ErrorDatabaseExpired   | File(s) are expired. Please make sure you are downloading and using the latest release version. (If using the Melissa Updater, check the bash script for 'RELEASE_VERSION = {version}' and change the release version if you are using an out of date release).     |
+| ErrorFoundOldFile   | File(s) are out of date. Please make sure you are downloading and using the latest release version. (If using the Melissa Updater, check the bash script for 'RELEASE_VERSION = {version}' and change the release version if you are using an out of date release).    |
+| ErrorLicenseExpired   | Expired license string. Please contact technical support below. |
+
+
+## Contact Us
+For free technical support, please call us at 800-MELISSA ext. 4 (800-635-4772 ext. 4) or email us at tech@melissa.com.
+
+To purchase this product, contact the Melissa sales department at 800-MELISSA ext. 3 (800-635-4772 ext. 3).
